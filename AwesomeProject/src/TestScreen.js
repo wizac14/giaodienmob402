1573348a59bded13c4c93a3e8f82c23580f972d6
luandeptrainhat <115625569+luandeptrainhat@users.noreply.github.com>
2023-08-02 21:04:16 +0700

    chức năng tìm kiếm lịch thi  + lịch học theo ngày

--- AwesomeProject/src/TestScreen.js
+++ AwesomeProject/src/TestScreen.js
@@ -6,22 +6,38 @@ import { FlatList } from 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { getLTUser } from './Heper/Service';
 import { UserContext } from './UserContext';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import moment from 'moment';
+import { getLTDate } from './Heper/Service';
 
 const TestScreen = (props) => {
   const [LH, setLH] = useState([]);
   const { id_user, setid_user } = useContext(UserContext);
+
   // const onGetLT = async () => {
   //   const LT = await getLT();
   //   console.log(LT.data);
   //   setLT(LT.data);
   // }
   const [LT, setLT] = useState([]);
-
+  const [today, setToday] = useState('');
   const ongetLTUser = async () => {
     // console.log('id_user',id_user);
     const L = await getLTUser(id_user);
     // console.log(LT.data);
     setLT(L.data);
+  }
+  const ongetLTDate = async (id_user, date) => {
+    const L = await getLTDate(id_user, date);
+    // console.log(LH.data);
+    setLT(L.data);
+  }
+
+  const handlePress = () => {
+    const date = moment().format('MM/DD/YYYY');
+    setToday(date);
+    console.log('Today is:', date);
+    ongetLTDate(id_user, date);
   }
 
   renderItem = ({ item }) => {
@@ -54,6 +70,9 @@ const TestScreen = (props) => {
     , [])
   return (
     <GestureHandlerRootView style={styles.container2}>
+      <TouchableOpacity onPress={handlePress} style={styles.getDay}>
+        <Text>Get Today's Date</Text>
+      </TouchableOpacity>
       <View style={styles.trendall}>
         <FlatList
           style={styles.container2}
@@ -79,7 +98,15 @@ const styles = StyleSheet.create({
     flex: 1,
 
   },
-
+  getDay: {
+    backgroundColor: 'red',
+    width: 100,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 10,
+  },
   cardView: {
     margin: 5,
     flexDirection: 'row',
