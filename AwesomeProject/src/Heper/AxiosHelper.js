0f95d53abbb3d8803a59a76e7f1f209cbd1ef564
luandeptrainhat <115625569+luandeptrainhat@users.noreply.github.com>
2023-09-30 17:14:14 +0700

    thêm xoay thẻ sinh viên

--- AwesomeProject/src/Heper/AxiosHelper.js
+++ AwesomeProject/src/Heper/AxiosHelper.js
@@ -3,8 +3,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AxiosInstance = (contentType = 'application/json') => {
     const axiosInstance = axios.create({
+// <<<<<<< Updated upstream
         // baseURL: 'http://192.168.128.13:3000/'
-         baseURL: 'http://192.168.1.6:3000/'
+         baseURL: 'http://192.168.1.253:3000/'
+// =======
+        // baseURL: 'http://192.168.1.253:3000/'
+// >>>>>>> Stashed changes
     });
 
     axiosInstance.interceptors.request.use(
--- AwesomeProject/src/Tet.js
+++ AwesomeProject/src/Tet.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { getCard } from './Heper/Service'
 import { useState, useEffect, useContext } from 'react';
@@ -24,15 +24,20 @@ const Tet = () => {
     // console.log("card--", card);
   }, [])
 
-  // const renderItem = ({ item }) => {
-  //   return (
-  //     <View>
-
-  //     </View>
-  //   );
-  // }
+  const [isRotated, setIsRotated] = useState(false);
+  const handleRotate = () => {
+    setIsRotated(!isRotated);
+  };
   return (
-    <View style={styles.body}>
+    // <View style={styles.body}>
+    //thêm để xoay màn hình
+    <View style={[styles.body]}>
+      <TouchableOpacity
+        style={styles.rotateButton}
+        onPress={handleRotate}
+      >
+        <Text style={styles.rotateButtonText}>Xoay</Text>
+      </TouchableOpacity>
 
       {card.length === 0 ? (
         <Text>Loading...</Text>
@@ -45,39 +50,38 @@ const Tet = () => {
           <Text> Giá trị đến : {card[0].worth_to} </Text>
            <Image source={{ uri: card[0].img }} style={{ width: 200, height: 200, borderRadius: 100 }} /> */}
 
-          <View style={styles.container}>
+          <View style={[styles.container, isRotated && styles.rotatedContainer]}>
             <View style={styles.header}>
-              <Image resizeMode='contain' style={styles.img} source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/FPT_Polytechnic.png/1200px-FPT_Polytechnic.png?20200416064027' }} />
+              <Image resizeMode='contain' style={[styles.avt]} source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/FPT_Polytechnic.png/1200px-FPT_Polytechnic.png?20200416064027' }} />
               <View style={styles.title1}>
                 <Text style={styles.school}>Trường Cao Đẳng FPT Polytechnic</Text>
                 <View style={styles.contTitle}><Text style={styles.title}>THẺ SINH VIÊN</Text></View>
               </View>
             </View>
             <View style={styles.main}>
-              <Image resizeMode='contain' style={styles.avt} source={{ uri: card[0].img }} />
+              <Image resizeMode='contain' style={[styles.avt, isRotated && styles.avt2]} source={{ uri: card[0].img }} />
               <View style={styles.info}>
                 <View style={styles.contname}>
-                  <Text style={styles.text}>Họ và tên </Text>
-                  <Text style={styles.name}>{card[0].name}</Text>
+                <Text style={[styles.text, isRotated && styles.rotatedText]}>Họ và tên/Name </Text>
+                  <Text style={[styles.name, isRotated && styles.rotatedText]}>{card[0].name}</Text>
                 </View>
                 <View style={styles.contMajor} >
-                  <Text style={styles.major}>{card[0].majors}</Text>
+                  <Text style={[styles.major, isRotated && styles.rotatedText]}>{card[0].majors}</Text>
                 </View>
                 <View style={styles.contMssv}>
-                  <Text style={styles.text}>MSSV/Student ID </Text>
-                  <Text style={styles.mssv}>{card[0].mssv}</Text>
+                  <Text style={[styles.text, isRotated && styles.rotatedText]}>MSSV/Student ID </Text>
+                  <Text style={[styles.mssv, isRotated && styles.rotatedText]}>{card[0].mssv}</Text>
                 </View>
                 <View style={styles.contWorth}>
                   <View >
-                    <Text style={styles.text}>Giá trị đến/Valid to </Text>
-                    <Text style={styles.worth}>{card[0].worth_to}</Text>
+                  <Text style={[styles.text, isRotated && styles.rotatedText]}>Giá trị đến/Valid to </Text>
+                    <Text style={[styles.worth, isRotated && styles.rotatedText]}>{card[0].worth_to}</Text>
                     <View style={styles.contCao}>
-                      <Text style={styles.caodang}>
+                      <Text style={[styles.caodang, isRotated && styles.caodang2]}>
                         caodang.fpt.edu.vn
                       </Text>
                     </View>
                   </View>
-
                   <Image resizeMode='contain' style={styles.ong} source={{ uri: 'https://caodang.fpt.edu.vn/wp-content/uploads/18198154_10208600482868814_3469513_n.png' }} />
                 </View>
 
@@ -85,25 +89,20 @@ const Tet = () => {
 
 
             </View>
-
-
           </View>
         </>
       )}
-
-
     </View>
   )
 }
-
 export default Tet
-
 const styles = StyleSheet.create({
   body: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     // backgroundColor: 'black',
+    // transform: [{ rotateZ: '90deg' }]
   },
   container: {
     width: '90%',
@@ -112,22 +111,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     borderRadius: 10,
     elevation: 20,
-    padding: 0,
-    margin: 0,
+    padding: 10,
+    // margin: 10,
+    // borderColor: 'gray',
+    // borderWidth: 1,
   },
   header: {
     width: '100%',
-
+    height: 80,
     flexDirection: 'row',
     // padding: 10,
     // backgroundColor: 'red',
     // justifyContent: 'center',
     // alignItems: 'center',
+    // borderColor: 'gray',
+    // borderWidth: 1,
 
   },
   img: {
     width: 100,
-
   },
   school: {
     fontSize: 8,
@@ -143,11 +145,12 @@ const styles = StyleSheet.create({
   },
   title1: {
     width: '70%',
-
-
+    height: 'auto',
     // justifyContent: 'center',
     alignItems: 'center',
     // backgroundColor: 'red',
+    // borderColor: 'gray',
+    // borderWidth: 1,
   },
   contTitle: {
     backgroundColor: 'orange',
@@ -158,21 +161,18 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 20,
     borderBottomRightRadius: 20,
     marginLeft: 10,
+    // borderColor: 'gray',
+    // borderWidth: 1,
   },
   avt: {
     height: 110,
     width: 100,
-    margin: 10,
+    
   },
   main: {
     width: '100%',
     flexDirection: 'row',
-    marginTop: 10,
-    // backgroundColor: 'red',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // borderColor: 'gray',
-    // borderWidth: 1,
+
   },
   text: {
     fontSize: 7,
@@ -232,11 +232,52 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-contMajor:{
-  width: '70%',
-  justifyContent: 'center',
-  // alignItems: 'center',
-  // backgroundColor: 'red',
-},
-
+  contMajor: {
+    width: '70%',
+    justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: 'red',
+  },
+  rotateButton: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  rotateButtonText: {
+    color: 'white',
+  },
+  rotatedContainer: {
+    transform: [{ rotateZ: '90deg' }],
+  },
+  rotatedContainer: {
+    width: '124%',  // Kích thước chiều rộng mới khi xoay
+    height: 300,
+    transform: [{ rotate: '90deg' }],
+    fontSize: 20,// Kích thước chiều cao mới khi xoay
+    padding: 10,
+    margin: 10,
+  },
+  rotatedText: {
+    fontSize: 10,
+    paddingVertical: 1// Kích thước font chữ mới
+  },
+  caodang2: {
+    width: 200,
+    fontSize: 6,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+  avt2: {
+    width: 120,
+  },
+  school2: {
+    fontSize: 8,
+    letterSpacing: 0.5,
+    fontWeight: 'bold',
+    color: 'black',
+  },
 })
